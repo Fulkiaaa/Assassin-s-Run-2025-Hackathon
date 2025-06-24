64c733be6b332e6b0d2c1d330af2de175fd1c62f
Clara Morin <clara.morin@icloud.com>
2025-06-24 11:57:15 +0200

    Initial commit

---
+++ app/src/main/java/com/jeu/endlessrunner/be/Constants.java
@@ -0,0 +1,19 @@
+package com.jeu.endlessrunner.be;
+
+import android.content.Context;
+
+public class Constants {
+
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+
+    public static int START_FROM_TOP = 50;
+
+    public static int MENU_SCENE = 0;
+    public static int GAME_SCENE = 1;
+
+    public static float SPEED = 30f;
+
+    public static Context CURRENT_CONTEXT;
+
+}
