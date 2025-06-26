88581357c4dbef602001ee5b8047e21ed3b4535a
Clara Morin <clara.morin@icloud.com>
2025-06-26 20:39:53 +0200

    package

--- app/src/androidTest/java/jeu/endlessrunner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/jeu/endlessrunner/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("vigneshgbe.endlessrunner", appContext.getPackageName());
+        assertEquals("jeu.endlessrunner", appContext.getPackageName());
     }
 }
